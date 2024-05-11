20a3e1c1a1a5684447bdf2bc173de75dbd6e6326
rupeshmishra90 <rupeshtikapur@gmail.com>
2024-05-11 20:44:43 +0530

    Remove RemoteFeedLoad from test to production

---
+++ MyFeed/Feed Feature/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,24 @@
+//
+//  RemoteFeedLoader.swift
+//  MyFeed
+//
+//  Created by Rupesh Kumar on 11/05/24.
+//
+
+import Foundation
+protocol HTTPClient{
+    func get(from url: URL)
+}
+public final class RemoteFeedLoader{
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient)
+    {
+        self.client = client
+        self.url = url
+    }
+    func load(){
+        client.get(from: url)
+    }
+}
